4906719ae0597c4709cae245587d27c07ed73d8c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-28 03:38:18 +0300

    Create constants.dart

---
+++ quranglow/lib/core/constants/constants.dart
@@ -0,0 +1,6 @@
+class CoreConstants {
+  static const appBoxName = 'quran_app_box';
+  static const lastReadKey = 'last_read';
+  static const editionKey = 'selected_edition';
+  static const cachePrefix = 'cache_';
+}
