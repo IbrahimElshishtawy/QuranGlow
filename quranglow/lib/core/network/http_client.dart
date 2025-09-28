ad84f026943c164c0c8e7410b7bc8b6bdc14b546
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-29 00:49:12 +0300

    Update http_client.dart

--- quranglow/lib/core/network/http_client.dart
+++ quranglow/lib/core/network/http_client.dart
@@ -7,17 +7,24 @@ class HttpClient {
 
   factory HttpClient({BaseOptions? options}) {
     final dio = Dio(
-      options ?? BaseOptions(connectTimeout: 15000, receiveTimeout: 15000),
+      options ??
+          BaseOptions(
+            connectTimeout: const Duration(milliseconds: 15000),
+            receiveTimeout: const Duration(milliseconds: 15000),
+            sendTimeout: const Duration(milliseconds: 15000),
+          ),
     );
+
     dio.interceptors.add(
       LogInterceptor(responseBody: false, requestBody: false),
     );
+
     // simple retry interceptor (1 retry)
     dio.interceptors.add(
       InterceptorsWrapper(
         onError: (e, handler) async {
           final r = e.requestOptions;
-          final retries = r.extra['retries'] as int? ?? 0;
+          final retries = (r.extra['retries'] as int?) ?? 0;
           if (retries < 1) {
             r.extra['retries'] = retries + 1;
             try {
@@ -28,14 +35,15 @@ class HttpClient {
                 options: Options(method: r.method, headers: r.headers),
               );
               return handler.resolve(resp);
-            } catch (err) {
-              L.e('HttpClient', err, e.stackTrace);
+            } catch (err, st) {
+              L.e('HttpClient', err, st);
             }
           }
           return handler.next(e);
         },
       ),
     );
+
     return HttpClient._(dio);
   }
 }
