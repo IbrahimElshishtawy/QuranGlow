1c9424c7876bd13dc151ef24748f1300111e166b
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-30 23:56:44 +0300

    Update last_read_card.dart

--- quranglow/lib/features/ui/pages/home/sections/last_read_card.dart
+++ quranglow/lib/features/ui/pages/home/sections/last_read_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quranglow/features/ui/pages/home/widgets/section_title.dart';
 import 'package:quranglow/features/ui/routes/app_routes.dart';
 
 class LastReadCard extends StatelessWidget {
