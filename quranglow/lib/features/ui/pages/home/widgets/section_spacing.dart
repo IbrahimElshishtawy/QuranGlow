ba05567f4a4fefc7d0f53c0a68cfc2985ecc21b5
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-30 23:51:33 +0300

    Create section_spacing.dart

---
+++ quranglow/lib/features/ui/pages/home/widgets/section_spacing.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SectionSpacing extends StatelessWidget {
+  final Widget child;
+  const SectionSpacing({super.key, required this.child});
+  @override
+  Widget build(BuildContext context) =>
+      Padding(padding: const EdgeInsets.fromLTRB(16, 16, 16, 0), child: child);
+}
