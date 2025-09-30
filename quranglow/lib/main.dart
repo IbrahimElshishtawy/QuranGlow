7d5ff330e441a6f44e49f341795213eae456bbbf
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-30 13:35:08 +0300

    Update main.dart

--- quranglow/lib/main.dart
+++ quranglow/lib/main.dart
@@ -1,38 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:quranglow/features/ui/pages/splash_screen.dart';
+import 'features/ui/routes/app_router.dart';
+import 'features/ui/routes/app_routes.dart';
 
-void main() {
-  runApp(const QuranGlowApp());
-}
+void main() => runApp(const QuranGlowApp());
 
 class QuranGlowApp extends StatelessWidget {
   const QuranGlowApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "QuranGlow",
+      title: 'QuranGlow',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
-      // أول شاشة هتظهر
-      home: const SplashScreen(),
-      routes: {
-        "/home": (context) => const HomeScreen(), // استبدلها بشاشتك الأساسية
-      },
-    );
-  }
-}
-
-/// دي شاشة مؤقتة عشان بعد الـ Splash
-/// استبدلها بالشاشة الرئيسية بتاعتك
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("QuranGlow Home")),
-      body: const Center(child: Text("Welcome to QuranGlow!")),
+      onGenerateRoute: onGenerateRoute,
+      initialRoute: AppRoutes.splash,
     );
   }
 }
