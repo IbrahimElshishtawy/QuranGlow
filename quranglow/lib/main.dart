b273b4e60e164ee446b740304ae02d68f925edd0
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-09-30 23:51:37 +0300

    Update main.dart

--- quranglow/lib/main.dart
+++ quranglow/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'core/theme/theme.dart';
 import 'features/ui/routes/app_router.dart';
 import 'features/ui/routes/app_routes.dart';
 
@@ -6,11 +7,14 @@ void main() => runApp(const QuranGlowApp());
 
 class QuranGlowApp extends StatelessWidget {
   const QuranGlowApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'QuranGlow',
       debugShowCheckedModeBanner: false,
+      theme: buildLightTheme(),
+      darkTheme: buildDarkTheme(),
       onGenerateRoute: onGenerateRoute,
       initialRoute: AppRoutes.splash,
     );
