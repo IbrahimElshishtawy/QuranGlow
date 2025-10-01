24d6a09ec273e43f476a278485d950f0f69c66b3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-01 17:32:44 +0300

    Update providers.dart

--- quranglow/lib/core/di/providers.dart
+++ quranglow/lib/core/di/providers.dart
@@ -1,23 +1,29 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
-import 'package:quranglow/core/api/alquran_cloud_source.dart';
-import 'package:quranglow/core/api/fawaz_cdn_source.dart';
-import 'package:quranglow/core/service/audio_service.dart';
-import 'package:quranglow/core/service/download_service.dart';
-import 'package:quranglow/core/service/quran_service.dart';
+import '../api/alquran_cloud_source.dart';
+import '../api/fawaz_cdn_source.dart';
+import '../service/quran_service.dart';
+import '../service/audio_service.dart';
+import '../service/download_service.dart';
 
 final httpClientProvider = Provider<http.Client>((ref) => http.Client());
-final fawazProvider = Provider(
+
+final fawazProvider = Provider<FawazCdnSource>(
   (ref) => FawazCdnSource(ref.watch(httpClientProvider)),
 );
-final alQuranProvider = Provider(
+
+final alQuranProvider = Provider<AlQuranCloudSource>(
   (ref) => AlQuranCloudSource(ref.watch(httpClientProvider)),
 );
-final quranServiceProvider = Provider(
+
+final quranServiceProvider = Provider<QuranService>(
   (ref) => QuranService(
     fawaz: ref.watch(fawazProvider),
     audio: ref.watch(alQuranProvider),
   ),
 );
-final audioServiceProvider = Provider((ref) => AudioService());
-final downloadServiceProvider = Provider((ref) => DownloadService());
+
+final audioServiceProvider = Provider<AudioService>((ref) => AudioService());
+final downloadServiceProvider = Provider<DownloadService>(
+  (ref) => DownloadService(),
+);
