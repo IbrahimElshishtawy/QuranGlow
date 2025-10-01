162f607172f54e24f9b5387c4698e1112e736f94
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-01 17:08:39 +0300

    Create app_routes.dart

---
+++ quranglow/lib/features/ui/routes/app_routes.dart
@@ -0,0 +1,17 @@
+class AppRoutes {
+  static const splash = '/';
+  static const home = '/home';
+  static const mushaf = '/mushaf';
+  static const surahs = '/surahs';
+  static const ayah = '/ayah';
+  static const player = '/player';
+  static const search = '/search';
+  static const bookmarks = '/bookmarks';
+  static const downloads = '/downloads';
+  static const downloadDetail = '/downloads/detail';
+  static const settings = '/settings';
+  static const goals = '/goals';
+  static const stats = '/stats';
+  static const onboarding = '/onboarding';
+  static const tafsir = '/tafsir';
+}
