fee8a5aa9effd527a41856735c1c76e9ce3cc858
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-02 14:55:07 +0300

    Update alquran_cloud_source.dart

--- quranglow/lib/core/api/alquran_cloud_source.dart
+++ quranglow/lib/core/api/alquran_cloud_source.dart
@@ -1,4 +1,6 @@
+// lib/core/api/alquran_cloud_source.dart
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 
 class AlQuranCloudSource {
   AlQuranCloudSource({required this.dio});
@@ -8,6 +10,7 @@ class AlQuranCloudSource {
 
   Future<List> listAudioEditions() async {
     final res = await dio.get('$_base/edition/format/audio');
+    debugPrint('[AQC] list editions ${res.statusCode}');
     if (res.statusCode != 200) {
       throw Exception('HTTP ${res.statusCode} عند جلب إصدارات الصوت');
     }
@@ -17,6 +20,7 @@ class AlQuranCloudSource {
 
   Future<Map<String, dynamic>> getSurahAudio(String edition, int s) async {
     final res = await dio.get('$_base/surah/$s/$edition');
+    debugPrint('[AQC] surah audio $s/$edition ${res.statusCode}');
     if (res.statusCode != 200) {
       throw Exception(
         'HTTP ${res.statusCode} عند جلب صوت السورة $s ($edition)',
