81e399f5f9a599ef62fc4a0e65178369906a296d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-03 12:35:52 +0300

    Update Goal.dart

--- quranglow/lib/core/model/Goal.dart
+++ quranglow/lib/core/model/Goal.dart
@@ -1,13 +1,37 @@
+// ignore_for_file: file_names
+
+import 'package:hive/hive.dart';
+
 class Goal {
-  final String title;
-  final double progress;
+  String title;
+  double progress;
+  Goal({required this.title, required this.progress});
+}
 
-  const Goal({required this.title, required this.progress});
+class GoalAdapter extends TypeAdapter<Goal> {
+  @override
+  final int typeId = 5;
 
-  factory Goal.fromMap(Map<String, dynamic> m) => Goal(
-    title: m['title'] as String,
-    progress: (m['progress'] as num).toDouble(),
-  );
+  @override
+  Goal read(BinaryReader reader) {
+    final fieldsCount = reader.readByte();
+    final fields = <int, dynamic>{};
+    for (var i = 0; i < fieldsCount; i++) {
+      fields[reader.readByte()] = reader.read();
+    }
+    return Goal(
+      title: fields[0] as String,
+      progress: (fields[1] as num).toDouble(),
+    );
+  }
 
-  Map<String, dynamic> toMap() => {'title': title, 'progress': progress};
+  @override
+  void write(BinaryWriter writer, Goal obj) {
+    writer
+      ..writeByte(2)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.progress);
+  }
 }
