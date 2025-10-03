2dc250680ae48d7d18d89ee2765438627b559367
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-04 02:01:46 +0300

    Create Play_list_State.dart

---
+++ quranglow/lib/core/model/Play_list_State.dart
@@ -0,0 +1,27 @@
+import 'package:just_audio/just_audio.dart';
+
+class PlaylistState {
+  final String editionId;
+  final int chapter;
+  final int total;
+  final Stream<Duration?> durationStream;
+  final Stream<Duration> positionStream;
+  final Stream<Duration> bufferedStream;
+  final Stream<int?> indexStream;
+  final Stream<bool> playingStream;
+  final Stream<LoopMode> loopModeStream;
+  final Stream<double> volumeStream;
+
+  const PlaylistState({
+    required this.editionId,
+    required this.chapter,
+    required this.total,
+    required this.durationStream,
+    required this.positionStream,
+    required this.bufferedStream,
+    required this.indexStream,
+    required this.playingStream,
+    required this.loopModeStream,
+    required this.volumeStream,
+  });
+}
