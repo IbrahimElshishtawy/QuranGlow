465269f00750787bfacc0014fa35cffa6393c55f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-05 22:36:49 +0300

    Update tafsir_explorer_page.dart

--- quranglow/lib/features/ui/pages/tafsir/tafsir_explorer_page.dart
+++ quranglow/lib/features/ui/pages/tafsir/tafsir_explorer_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: unused_result
+// ignore_for_file: unused_result, no_leading_underscores_for_local_identifiers
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
