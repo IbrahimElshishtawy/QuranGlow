37854d735a1487375c79a1bcdbb618ba76d20f74
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-05 03:31:59 +0300

    Update settings_providers.dart

--- quranglow/lib/features/ui/pages/setting/widgets/settings_providers.dart
+++ quranglow/lib/features/ui/pages/setting/widgets/settings_providers.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_riverpod/legacy.dart';
 
-// حالياً StateProviders بسيطة (لو عايز نخزنها دايمًا نربط SharedPrefs لاحقًا)
-final notificationsEnabledProvider = StateProvider<bool>((_) => false);
+/// تفعيل/وقت التذكير اليومي
+final notificationsEnabledProvider = StateProvider<bool>((ref) => false);
 final reminderTimeProvider = StateProvider<TimeOfDay>(
-  (_) => const TimeOfDay(hour: 7, minute: 30),
+  (ref) => const TimeOfDay(hour: 7, minute: 30),
 );
 
-final keepScreenOnProvider = StateProvider<bool>((_) => false);
-final useCellularProvider = StateProvider<bool>((_) => true);
-final hapticsProvider = StateProvider<bool>((_) => true);
-final reduceMotionProvider = StateProvider<bool>((_) => false);
+/// تفعيل/وقت ترديد الصلاة على النبي ﷺ
+final salawatEnabledProvider = StateProvider<bool>((ref) => false);
+final salawatTimeProvider = StateProvider<TimeOfDay>(
+  (ref) => const TimeOfDay(hour: 12, minute: 0),
+);
+
+/// (اختياري) إعدادات استخدام
+final keepScreenOnProvider = StateProvider<bool>((ref) => false);
+final useCellularProvider = StateProvider<bool>((ref) => true);
+final hapticsProvider = StateProvider<bool>((ref) => true);
+final reduceMotionProvider = StateProvider<bool>((ref) => false);
