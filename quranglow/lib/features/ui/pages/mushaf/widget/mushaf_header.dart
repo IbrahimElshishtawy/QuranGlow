1f15d55a3b85fc649ed670985733325a92d9fad2
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-06 03:28:28 +0300

    Update mushaf_header.dart

--- quranglow/lib/features/ui/pages/mushaf/widget/mushaf_header.dart
+++ quranglow/lib/features/ui/pages/mushaf/widget/mushaf_header.dart
@@ -1,9 +1,9 @@
-// lib/features/ui/pages/mushaf/widgets/mushaf_header.dart
 import 'package:flutter/material.dart';
 
 class MushafHeader extends StatelessWidget {
   const MushafHeader({super.key, required this.surahName});
   final String surahName;
+
   @override
   Widget build(BuildContext context) {
     return Text(
