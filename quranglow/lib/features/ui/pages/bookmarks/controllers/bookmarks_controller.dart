f37ac5b398c3ed878421bba63368a6be30a904c4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-07 02:16:31 +0300

    Create bookmarks_controller.dart

---
+++ quranglow/lib/features/ui/pages/bookmarks/controllers/bookmarks_controller.dart
@@ -0,0 +1,10 @@
+import 'package:quranglow/core/model/bookmark.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+class BookmarksController extends StateNotifier<List<Bookmark>> {
+  BookmarksController() : super(const []);
+
+  void add(Bookmark b) => state = [...state, b];
+  void removeAt(int i) => state = [...state]..removeAt(i);
+  void clearAll() => state = const [];
+}
