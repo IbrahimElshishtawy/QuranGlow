87357b2c0cf7dd8d9aaf47df4dc0dcd988ef4d54
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-06 03:06:12 +0300

    Update app_routes.dart

--- quranglow/lib/features/ui/routes/app_routes.dart
+++ quranglow/lib/features/ui/routes/app_routes.dart
@@ -17,3 +17,17 @@ class AppRoutes {
   static const onboarding = '/onboarding';
   static const tafsir = '/tafsir';
 }
+
+class TafsirArgs {
+  final int surah;
+  final int ayah;
+  final String? editionId;
+  final String? editionName;
+
+  const TafsirArgs({
+    required this.surah,
+    required this.ayah,
+    this.editionId,
+    this.editionName,
+  });
+}
