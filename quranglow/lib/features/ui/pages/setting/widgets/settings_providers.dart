471b42c71dde097bdef91f854097c362243ed7aa
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-08 00:26:44 +0300

    Update settings_providers.dart

--- quranglow/lib/features/ui/pages/setting/widgets/settings_providers.dart
+++ quranglow/lib/features/ui/pages/setting/widgets/settings_providers.dart
@@ -1,20 +1,17 @@
+// lib/features/ui/pages/setting/widgets/settings_providers.dart
 import 'package:flutter/material.dart';
-
 import 'package:flutter_riverpod/legacy.dart';
 
-/// تفعيل/وقت التذكير اليومي
 final notificationsEnabledProvider = StateProvider<bool>((ref) => false);
 final reminderTimeProvider = StateProvider<TimeOfDay>(
   (ref) => const TimeOfDay(hour: 7, minute: 30),
 );
 
-/// تفعيل/وقت ترديد الصلاة على النبي ﷺ
 final salawatEnabledProvider = StateProvider<bool>((ref) => false);
 final salawatTimeProvider = StateProvider<TimeOfDay>(
   (ref) => const TimeOfDay(hour: 12, minute: 0),
 );
 
-/// (اختياري) إعدادات استخدام
 final keepScreenOnProvider = StateProvider<bool>((ref) => false);
 final useCellularProvider = StateProvider<bool>((ref) => true);
 final hapticsProvider = StateProvider<bool>((ref) => true);
