a70959e6119511df977c1c1cecbc333dee8b7b82
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-08 00:55:01 +0300

    Create empty_state.dart

---
+++ quranglow/lib/features/ui/pages/search/views/empty_state.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class EmptyState extends StatelessWidget {
+  const EmptyState({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('لا توجد نتائج مطابقة'));
+  }
+}
