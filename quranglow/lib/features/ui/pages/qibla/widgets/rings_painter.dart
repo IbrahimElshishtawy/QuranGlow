3aff5935973947a47e03e4f92d130d8ab0a7a242
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-11 20:44:40 +0300

    Create rings_painter.dart

---
+++ quranglow/lib/features/ui/pages/qibla/widgets/rings_painter.dart
@@ -0,0 +1,22 @@
+// lib/features/ui/pages/qibla/painters/rings_painter.dart
+import 'package:flutter/material.dart';
+
+class RingsPainter extends CustomPainter {
+  final Color color;
+  const RingsPainter(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final p = Paint()
+      ..color = color.withOpacity(.25)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 1;
+    final c = size.width / 2;
+    for (var i = 1; i <= 3; i++) {
+      canvas.drawCircle(Offset(c, c), c * i / 3, p);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant RingsPainter oldDelegate) => false;
+}
