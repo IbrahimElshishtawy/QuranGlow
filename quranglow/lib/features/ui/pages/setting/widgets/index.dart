614eeb9e06abe357fd7124744c3ce53f03f446e4
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-11 13:20:58 +0300

    Update index.dart

--- quranglow/lib/features/ui/pages/setting/widgets/index.dart
+++ quranglow/lib/features/ui/pages/setting/widgets/index.dart
@@ -1,7 +1,6 @@
 export 'appearance_section.dart';
 export 'goals_section.dart';
 export 'notifications_section.dart';
-export 'usage_section.dart';
 export 'ayah_of_day_section.dart';
 export 'section_header.dart';
 export 'font_scale_dialog.dart';
