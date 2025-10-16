ebb90189dfc5e4690f49af93610f4669b6416ede
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-16 15:09:08 +0300

    Update selection_card.dart

--- quranglow/lib/features/ui/pages/tafsir/widget/selection_card.dart
+++ quranglow/lib/features/ui/pages/tafsir/widget/selection_card.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quranglow/core/model/book/surah.dart';
-
 import 'ayah_picker.dart';
 
 class SelectionCard extends StatelessWidget {
