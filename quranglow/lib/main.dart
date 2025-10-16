4a3367c701d901b1f27a29dbffa4dee05bc106b3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-16 11:53:56 +0300

    Update main.dart

--- quranglow/lib/main.dart
+++ quranglow/lib/main.dart
@@ -1,4 +1,3 @@
-// lib/main.dart
 // ignore_for_file: depend_on_referenced_packages, unnecessary_underscores
 
 import 'package:flutter/material.dart';
@@ -6,8 +5,13 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:quranglow/core/service/setting/notification_service.dart';
+import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
+import 'package:quranglow/core/service/quran/quran_service.dart';
 
+import 'package:quranglow/core/service/setting/notification_service.dart';
+import 'package:quranglow/core/api/alquran_cloud_source.dart';
+import 'package:quranglow/core/api/fawaz_cdn_source.dart';
 import 'core/di/providers.dart';
 import 'core/theme/theme.dart';
 import 'features/ui/pages/spa/splash_screen.dart';
@@ -18,8 +22,20 @@ Future<void> main() async {
   FlutterNativeSplash.preserve(widgetsBinding: binding);
   await Hive.initFlutter();
   await NotificationService.instance.init();
+  try {
+    final dio = Dio();
+    final cloud = AlQuranCloudSource(dio: dio);
+    final fawaz = FawazCdnSource(http.Client(), dio);
+    final service = QuranService(fawaz: fawaz, cloud: cloud, audio: cloud);
 
+    debugPrint('⬇️ جاري تحميل جميع السور لأول مرة...');
+    await service.getQuranAllText('quran-uthmani');
+    debugPrint('✅ تم حفظ جميع السور في التخزين المحلي (Hive)');
+  } catch (e) {
+    debugPrint('⚠️ فشل تحميل بعض السور: $e');
+  }
   runApp(const ProviderScope(child: QuranGlowApp()));
+
   WidgetsBinding.instance.addPostFrameCallback((_) {
     FlutterNativeSplash.remove();
   });
@@ -44,8 +60,8 @@ class QuranGlowApp extends ConsumerWidget {
         debugShowCheckedModeBanner: false,
         theme: buildLightTheme(fontFamily: 'System', fontScale: 1),
         darkTheme: buildDarkTheme(fontFamily: 'System', fontScale: 1),
-        localizationsDelegates: _delegates, // ← add
-        supportedLocales: _locales, // ← add
+        localizationsDelegates: _delegates,
+        supportedLocales: _locales,
         home: const Scaffold(
           backgroundColor: Colors.white,
           body: Center(child: CircularProgressIndicator()),
