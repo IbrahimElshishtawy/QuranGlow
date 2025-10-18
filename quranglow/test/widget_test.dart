75604eaee4ef3d230a79d09b486a172ff3e072c3
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-10-18 13:12:46 +0300

    Update widget_test.dart

--- quranglow/test/widget_test.dart
+++ quranglow/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:quranglow/main.dart';
+import 'package:quranglow/Quran_Glow_App.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
